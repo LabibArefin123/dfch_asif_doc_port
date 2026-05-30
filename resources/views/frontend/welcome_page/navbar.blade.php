ef5ef90c1f2567648ff34906a6ef93a732603e23
LabibArefin123 <ah947777@gmail.com>
2026-05-31 00:57:26 +0600

    navbar fixed issue

--- resources/views/frontend/welcome_page/navbar.blade.php
+++ resources/views/frontend/welcome_page/navbar.blade.php
@@ -5,12 +5,11 @@
 <link rel="stylesheet" href="{{ asset('css/frontend/navbar/navbar-button.css') }}">
 <link rel="stylesheet" href="{{ asset('css/frontend/navbar/navbar-mobile.css') }}">
 <link rel="stylesheet" href="{{ asset('css/frontend/navbar/navbar-overlay.css') }}">
-
 <nav class="navbar portfolio-navbar fixed-top">
 
-    <div class="container">
+    <div class="container navbar-wrapper">
 
-        {{-- Brand --}}
+        <!-- LEFT : BRAND -->
         <a href="{{ route('welcome') }}" class="navbar-brand d-flex align-items-center">
 
             <img src="{{ asset('uploads/images/icon.png') }}" alt="Logo" class="brand-logo">
@@ -27,146 +26,91 @@
 
         </a>
 
-        {{-- Mobile Menu Button --}}
-        <button id="navbarOpenBtn" class="navbar-toggler d-lg-none" type="button">
+        <!-- CENTER : DESKTOP MENU -->
+        <ul class="navbar-nav portfolio-menu desktop-menu d-none d-lg-flex">
 
-            <i class="fas fa-bars"></i>
+            <li class="nav-item">
+                <a href="{{ route('welcome') }}" class="nav-link">
+                    Home
+                </a>
+            </li>
 
-        </button>
+            <li class="nav-item">
+                <a href="{{ route('about') }}" class="nav-link">
+                    About
+                </a>
+            </li>
 
-        {{-- Desktop Button --}}
-        <a href="{{ route('contact') }}" class="btn portfolio-btn d-none d-lg-inline-flex">
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle">
+                    Professional Profile
+                </a>
 
-            Book Appointment
+                <ul class="dropdown-menu">
+                    <li><a href="{{ route('page_1') }}" class="dropdown-item">Educational Background</a></li>
+                    <li><a href="{{ route('page_2') }}" class="dropdown-item">International Conference</a></li>
+                    <li><a href="{{ route('page_3') }}" class="dropdown-item">Journal Publication</a></li>
+                    <li><a href="{{ route('page_4') }}" class="dropdown-item">Membership</a></li>
+                </ul>
+            </li>
 
-        </a>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle">
+                    Conditions We Treat
+                </a>
 
-    </div>
+                <ul class="dropdown-menu">
+                    <li><a href="{{ route('piles') }}" class="dropdown-item">Piles</a></li>
+                    <li><a href="{{ route('fissure') }}" class="dropdown-item">Fissure</a></li>
+                    <li><a href="{{ route('fistula') }}" class="dropdown-item">Fistula</a></li>
+                    <li><a href="{{ route('ibs') }}" class="dropdown-item">IBS</a></li>
+                    <li><a href="{{ route('colorectal_cancer') }}" class="dropdown-item">Colorectal Cancer</a></li>
+                </ul>
+            </li>
 
-</nav>
+            <li class="nav-item">
+                <a href="{{ route('gallery') }}" class="nav-link">
+                    Gallery
+                </a>
+            </li>
 
-{{-- Overlay --}}
-<div class="navbar-overlay"></div>
+            <li class="nav-item">
+                <a href="{{ route('book') }}" class="nav-link">
+                    Book
+                </a>
+            </li>
 
-{{-- Drawer --}}
-<div class="navbar-collapse" id="navbarCollapse">
+            <li class="nav-item">
+                <a href="{{ route('faq') }}" class="nav-link">
+                    FAQ
+                </a>
+            </li>
 
-    <div class="drawer-header">
+            <li class="nav-item">
+                <a href="{{ route('contact') }}" class="nav-link">
+                    Contact
+                </a>
+            </li>
 
-        <h5>Menu</h5>
+        </ul>
 
-        <button id="navbarCloseBtn" class="navbar-close-btn">
+        <!-- RIGHT -->
+        <div class="navbar-actions">
 
-            <i class="fas fa-times"></i>
-
-        </button>
-
-    </div>
-
-    <ul class="navbar-nav portfolio-menu">
-
-        <li class="nav-item">
-            <a href="{{ route('welcome') }}" class="nav-link {{ request()->routeIs('welcome') ? 'active' : '' }}">
-                Home
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a href="{{ route('about') }}" class="nav-link {{ request()->routeIs('about') ? 'active' : '' }}">
-                About
-            </a>
-        </li>
-
-        <li class="nav-item dropdown" id="profile_dropdown">
-
-            <a href="#" class="nav-link dropdown-toggle">
-                Professional Profile
-            </a>
-
-            <ul class="dropdown-menu">
-
-                <li>
-                    <a href="{{ route('page_1') }}" class="dropdown-item">
-                        Educational Background
-                    </a>
-                </li>
-
-                <li>
-                    <a href="{{ route('page_2') }}" class="dropdown-item">
-                        International Conference
-                    </a>
-                </li>
-
-                <li>
-                    <a href="{{ route('page_3') }}" class="dropdown-item">
-                        Journal Publication
-                    </a>
-                </li>
-
-                <li>
-                    <a href="{{ route('page_4') }}" class="dropdown-item">
-                        Membership
-                    </a>
-                </li>
-
-            </ul>
-
-        </li>
-
-        <li class="nav-item dropdown" id="conditions_dropdown">
-
-            <a href="#" class="nav-link dropdown-toggle">
-                Conditions We Treat
-            </a>
-
-            <ul class="dropdown-menu">
-
-                <li><a href="{{ route('piles') }}" class="dropdown-item">Piles</a></li>
-                <li><a href="{{ route('fissure') }}" class="dropdown-item">Fissure</a></li>
-                <li><a href="{{ route('fistula') }}" class="dropdown-item">Fistula</a></li>
-                <li><a href="{{ route('ibs') }}" class="dropdown-item">IBS</a></li>
-                <li><a href="{{ route('colorectal_cancer') }}" class="dropdown-item">Colorectal Cancer</a></li>
-
-            </ul>
-
-        </li>
-
-        <li class="nav-item">
-            <a href="{{ route('gallery') }}" class="nav-link">
-                Gallery
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a href="{{ route('book') }}" class="nav-link">
-                Book
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a href="{{ route('faq') }}" class="nav-link">
-                FAQ
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a href="{{ route('contact') }}" class="nav-link">
-                Contact
-            </a>
-        </li>
-
-        <!-- Mobile CTA -->
-
-        <li class="nav-item mt-3">
-
-            <a href="{{ route('contact') }}" class="btn portfolio-btn w-100">
+            <a href="{{ route('contact') }}" class="btn portfolio-btn">
 
                 Book Appointment
 
             </a>
 
-        </li>
+            <button id="navbarOpenBtn" class="navbar-toggler d-lg-none" type="button">
 
-    </ul>
+                <i class="fas fa-bars"></i>
 
-</div>
+            </button>
+
+        </div>
+
+    </div>
+
+</nav>
